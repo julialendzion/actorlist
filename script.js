1d0d8d3cbea1c6688986637538e64e598686731e
Julia Lendzion <juli47g3@stud.kea.dk>
2021-08-25 19:28:42 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+fetch("actors.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    showData(data);
+  });
+
+function showData(data) {
+  console.log("data", data);
+  data.forEach(actor);
+}
+function actor(data) {
+  //grab the template
+
+  const template = document.querySelector("template").content;
+  //clone
+  const copy = template.cloneNode(true);
+
+  //adjust stuff
+  copy.querySelector(".fullname").textContent = data.fullname;
+  copy.querySelector(".name").addEventListener("click", showDetails);
+  //apend it
+  document.querySelector("#actorlist").appendChild(copy);
+
+  function showDetails() {
+    console.log("dziala!!");
+    document.querySelector(".details").classList.remove("hidden");
+    document.querySelector(".movie").textContent = data.movie;
+  }
+}
